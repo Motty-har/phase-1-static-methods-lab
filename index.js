4e7d7b3d5e8bf19f3846a5d0af52e11671ae707d
Motty-har <harligmotty@gmail.com>
2023-06-01 18:36:16 -0400

    Finished

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+ 
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    const strs = new Set(['THE', 'A', 'AN', 'BUT', 'OF', 'AND', 'FOR', 'AT', 'BY', 'FROM'])
+    const sentence = string.split(" ").map((word) => 
+    strs.has(word.toUpperCase())
+    ? word.toLowerCase() : word[0].toUpperCase() + word.slice(1).split().map((letter) => letter.toLowerCase()).join(""))
+    const newSentence = sentence.join(" ")
+    return this.capitalize(newSentence)
+
+  }
 }
+
+
+
+
+const strs = new Set(["ABOUT", "UNICORN", "TECHNOLOGIES"]),
+sentence = "ANDRII IS GOOD IN JS TECHNOLOGIES",
+res = sentence.split(" ").map((w) =>
+strs.has(w.toUpperCase())
+? w.toLowerCase()
+: w[0].toUpperCase() + w.slice(1).split().map((c) => c.toLowerCase())
+)
+.join(" ");
+
+console.log(res)
